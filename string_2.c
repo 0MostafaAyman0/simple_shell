b4496f5233c8ab5ecb0426b32236e7d5f6e230b9
Mostafa_Ayman <mostafayma123@gmail.com>
2023-08-18 10:12:26 -0700

    update

--- string_2.c
+++ string_2.c
@@ -2,106 +2,106 @@
 
 /**
  * _strcat - concatenate two strings
- * @dest: char pointer the dest of the copied str
- * @src: const char pointer the source of str
- * Return: the dest
+ * @mos: char pointer the dest of the copied str
+ * @off: const char pointer the source of str
+ * Return: the mos
  */
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *mos, const char *off)
 {
-	int i;
+	int f;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (f = 0; mos[f] != '\0'; f++)
 		;
 
-	for (j = 0; src[j] != '\0'; j++)
+	for (j = 0; off[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
-		i++;
+		mos[f] = src[j];
+		f++;
 	}
 
-	dest[i] = '\0';
-	return (dest);
+	mos[f] = '\0';
+	return (mos);
 }
 /**
  * *_strcpy - Copies the string pointed to by src.
- * @dest: Type char pointer the dest of the copied str
- * @src: Type char pointer the source of str
- * Return: the dest.
+ * @mos: Type char pointer the dest of the copied str
+ * @off: Type char pointer the source of str
+ * Return: the mos.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *mos, char *off)
 {
 
-	size_t i;
+	size_t q;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (q = 0; off[q] != '\0'; q++)
 	{
-		dest[i] = src[i];
+		mos[q] = off[q];
 	}
-	dest[i] = '\0';
+	mos[q] = '\0';
 
-	return (dest);
+	return (mos);
 }
 /**
  * _strcmp - Function that compares two strings.
- * @s1: type str compared
- * @s2: type str compared
+ * @str1: type str compared
+ * @str2: type str compared
  * Return: Always 0.
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *str1, char *str2)
 {
-	int i;
+	int w;
 
-	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+	for (w = 0; str1[w] == str2[w] && str1[w]; w++)
 	;
 
-	if (s1[i] > s2[i])
+	if (str1[w] > str2[w])
 		return (1);
-	if (s1[i] < s2[i])
+	if (str1[w] < str2[w])
 		return (-1);
 	return (0);
 }
 /**
  * _strchr - locates a character in a string,
- * @s: string.
- * @c: character.
- * Return: the pointer to the first occurrence of the character c.
+ * @r: string.
+ * @t: character.
+ * Return: the pointer to the first occurrence of the character t.
  */
-char *_strchr(char *s, char c)
+char *_strchr(char *r, char t)
 {
-	unsigned int i = 0;
+	unsigned int m = 0;
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
+	for (; *(r + m) != '\0'; m++)
+		if (*(r + m) == t)
+			return (r + m);
+	if (*(r + m) == t)
+		return (r + m);
 	return ('\0');
 }
 /**
  * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
+ * @i: initial segment.
+ * @acc: accepted bytes.
  * Return: the number of accepted bytes.
  */
-int _strspn(char *s, char *accept)
+int _strspn(char *i, char *acc)
 {
-	int i, j, bool;
+	int o, p, exx;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (o = 0; *(i + o) != '\0'; o++)
 	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; j++)
+		exx = 1;
+		for (p = 0; *(acc + p) != '\0'; p++)
 	{
-			if (*(s + i) == *(accept + j))
+			if (*(i + o) == *(acc + p))
 			{
-				bool = 0;
+				exx = 0;
 				break;
 			}
 	}
-		if (bool == 1)
-		break;
+	if (exx == 1)
+	break;
 	}
-	return (i);
+	return (o);
 }
 
